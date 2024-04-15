0e892ed6355130c371e6e6510f34efa4bdcd3a53
Atharv <atharvpathak248@gmail.com>
2024-04-15 15:57:49 +0530

    Added Bootstrap and Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className='navbar'>
+      <div className='container-fluid'>
+        <a className='navbar-brand'>Password Manager </a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
