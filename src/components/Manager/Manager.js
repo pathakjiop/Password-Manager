1eb0cadc1b8eff3f6c5ff7d225959c405fe53bfd
Atharv <atharvpathak248@gmail.com>
2024-04-16 18:35:14 +0530

    Added Background

--- src/components/Manager/Manager.js
+++ src/components/Manager/Manager.js
@@ -3,19 +3,7 @@ import React from "react";
 const Manager = () => {
   return (
     <>
-      <div class='absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]'></div>
-
-      <div className=' text-white flex flex-col p-4'>
-        <input
-          type='text'
-          name=''
-          id=''
-        />
-        <div className='flex '>
-          <input type='text' />
-          <input type='text' />
-        </div>
-      </div>
+      <div class='absolute top-0 z-[-2] h-screen w-screen bg-slate-900 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]'></div>
     </>
   );
 };
